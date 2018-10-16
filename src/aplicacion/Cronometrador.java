293ab5c9432127ce0bba578287b037d01d40b8f5
SabrinaGar <sabrina.garcia@correo.unimet.edu.ve>
2018-10-16 00:40:08 -0400

    OLVIDE HACER PULL PRIMERO x_X

--- src/aplicacion/Cronometrador.java
+++ src/aplicacion/Cronometrador.java
@@ -6,13 +6,10 @@ import javax.swing.JTextField;
 public class Cronometrador extends Thread {
 
     private Semaphore semContMutEx;//Semaforo para establecer exclusion mutua entre el cronometrador y el gerente
-<<<<<<< HEAD
+
     private int cont;//contador ****** seccion critica¿?
-    private int duracionDia;//tiempo en segundos que dura un dia en el programa****(deberia pedirse desde el txt de fabrica)
-=======
-    private int cont;//contador
-    private double duracionDia;//tiempo en segundos que dura un dia en el programa
->>>>>>> da67cdf0fdfedc1a82b369878aa7052222ad4680
+    private  double duracionDia;//tiempo en segundos que dura un dia en el programa****(deberia pedirse desde el txt de fabrica)
+
     private int diasDespacho;//cantidad de dias entre despachos
     private JTextField statusCTextField;//TextField donde se muestra el status del cronometrador
     private JTextField diasDespachoTextField;//TextField donde se muestra los dias que faltan para el despacho
